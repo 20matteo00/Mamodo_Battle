73b7af27e06a89c1c7a09e39ed15317a9eb62e7d
20matteo00 <71841486+20matteo00@users.noreply.github.com>
2023-02-21 14:03:39 +0100

    Add files via upload

---
+++ SQL/film.sql
@@ -0,0 +1,5 @@
+insert into film values (1, "FILM 1 - Il Mamodo non elencato N.101");
+insert into film values (2, "FILM 2 - L'Attacco dei Mega-Vulcan");
+insert into film values (3, "OAV 1 - Corri Zatch! Ponygon e' stato rapito!");
+insert into film values (4, "OAV 2 - 00F, Il ladro di tette d'oro");
+insert into film values (5, "Special - Chichi wo moge");
